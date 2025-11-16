04370d66160e3b02a9dc69099f47dee9744da68c
NeyserRivas <RR23072@ues.edu.sv>
2025-11-15 18:05:24 -0600

    Formulario de reportes finalizado

--- SistemaReservaSalas/Formularios/frmDisponibilidadSalas.Designer.cs
+++ SistemaReservaSalas/Formularios/frmDisponibilidadSalas.Designer.cs
@@ -31,9 +31,20 @@ namespace SistemaReservaSalas
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbSalaCalendario = new System.Windows.Forms.ComboBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblInfoSala = new System.Windows.Forms.Label();
+            this.lblFechaSeleccionada = new System.Windows.Forms.Label();
+            this.dgvHorarios = new System.Windows.Forms.DataGridView();
+            this.btnNuevaReserva = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHorarios)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,8 +60,8 @@ namespace SistemaReservaSalas
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.cmbSalaCalendario);
+            this.groupBox1.Controls.Add(this.btnActualizar);
             this.groupBox1.Location = new System.Drawing.Point(182, 56);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(329, 78);
@@ -58,34 +69,113 @@ namespace SistemaReservaSalas
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Selección ";
             // 
-            // button1
+            // cmbSalaCalendario
             // 
-            this.button1.Location = new System.Drawing.Point(201, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cmbSalaCalendario.FormattingEnabled = true;
+            this.cmbSalaCalendario.Location = new System.Drawing.Point(23, 32);
+            this.cmbSalaCalendario.Name = "cmbSalaCalendario";
+            this.cmbSalaCalendario.Size = new System.Drawing.Size(121, 22);
+            this.cmbSalaCalendario.TabIndex = 1;
             // 
-            // comboBox1
+            // btnActualizar
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(23, 32);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 22);
-            this.comboBox1.TabIndex = 1;
+            this.btnActualizar.Location = new System.Drawing.Point(201, 32);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 0;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(40, 157);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvHorarios);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Location = new System.Drawing.Point(342, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(354, 278);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblFechaSeleccionada);
+            this.groupBox3.Controls.Add(this.lblInfoSala);
+            this.groupBox3.Location = new System.Drawing.Point(18, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(320, 76);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mostrando disponibilidad para:";
+            // 
+            // lblInfoSala
+            // 
+            this.lblInfoSala.AutoSize = true;
+            this.lblInfoSala.Location = new System.Drawing.Point(32, 18);
+            this.lblInfoSala.Name = "lblInfoSala";
+            this.lblInfoSala.Size = new System.Drawing.Size(31, 14);
+            this.lblInfoSala.TabIndex = 0;
+            this.lblInfoSala.Text = "Sala:";
+            // 
+            // lblFechaSeleccionada
+            // 
+            this.lblFechaSeleccionada.AutoSize = true;
+            this.lblFechaSeleccionada.Location = new System.Drawing.Point(32, 44);
+            this.lblFechaSeleccionada.Name = "lblFechaSeleccionada";
+            this.lblFechaSeleccionada.Size = new System.Drawing.Size(40, 14);
+            this.lblFechaSeleccionada.TabIndex = 1;
+            this.lblFechaSeleccionada.Text = "Fecha:";
+            // 
+            // dgvHorarios
+            // 
+            this.dgvHorarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHorarios.Location = new System.Drawing.Point(18, 104);
+            this.dgvHorarios.Name = "dgvHorarios";
+            this.dgvHorarios.Size = new System.Drawing.Size(320, 150);
+            this.dgvHorarios.TabIndex = 1;
+            // 
+            // btnNuevaReserva
+            // 
+            this.btnNuevaReserva.Location = new System.Drawing.Point(191, 479);
+            this.btnNuevaReserva.Name = "btnNuevaReserva";
+            this.btnNuevaReserva.Size = new System.Drawing.Size(135, 43);
+            this.btnNuevaReserva.TabIndex = 4;
+            this.btnNuevaReserva.Text = "Nueva Reserva";
+            this.btnNuevaReserva.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(360, 479);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(135, 43);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
             // 
             // frmDisponibilidadSalas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 485);
+            this.ClientSize = new System.Drawing.Size(727, 567);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnNuevaReserva);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmDisponibilidadSalas";
             this.Text = "Disponibilidad de Salas";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHorarios)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -94,7 +184,15 @@ namespace SistemaReservaSalas
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbSalaCalendario;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvHorarios;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblFechaSeleccionada;
+        private System.Windows.Forms.Label lblInfoSala;
+        private System.Windows.Forms.Button btnNuevaReserva;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
